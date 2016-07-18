ea050f2701718c81eba7ebccf628b441e369bfd1
Nicolas Forgerit <hello@nicolasforgerit.com>
2016-07-18 07:50:50 +0200

    First version

---
+++ README.md
@@ -0,0 +1,30 @@
+FIFO-Queue
+
+FIFO (First In First Out) Queues are one of the basic data structures in Software Engineering.
+They come in handy for many different use cases, e.g. Job Queuing where
+a FIFO Queue would manage a Collection of Jobs which need to be processed
+in a sequential order. Other use cases could be Flash Messagers for web
+pages or multi-tier collection synchronizers which subsequently send locally
+updated items to remote tiers.
+
+This package is abstract and can be used as core data structure of the afore-
+mentioned use cases. It aims for correctness, performance and reusability.
+
+# Usage
+    npm i --save fifo-queue
+    const queue = require('fifo-queue');
+    //@TODO: Add basic usage
+
+# More examples
+// @TODO: Add more examples
+- bulk set and get
+- filter w/ simple obj
+- filter w/ $lt/$gt operator
+
+# Testing
+
+# Performance
+
+# Author
+https://nicolasforgerit.com/projects/fifo-queue
+Nicolas Forgerit <hello@nicolasforgerit.com>
---
+++ index.js
@@ -0,0 +1,168 @@
+const _ = require('lodash');
+
+/**
+ * JobQueue using FIFO strategy.
+ */
+class FifoQueue {
+
+    /**
+     * Basic constructor which may optionally process a given
+     * initial set of items.
+     *
+     * @param initialSet
+     */
+    constructor(initialSet) {
+        this.itemSet = Array.isArray(initialSet) ? initialSet : [];
+    }
+
+    /**
+     * Set all items at once.
+     *
+     * @param itemSet
+     */
+    setItems(itemSet) {
+        this.itemSet = itemSet;
+    }
+
+    /**
+     * Get all items at once.
+     *
+     * @returns {*|Array}
+     */
+    getItems() {
+        return this.itemSet;
+    }
+
+    /**
+     * Add a single item to the FIFO Queue.
+     *
+     * @param item
+     */
+    addItem(item) {
+        this.itemSet.unshift(item);
+    }
+
+    /**
+     * Acquire next item of the FIFO Queue.
+     *
+     * @returns {T}
+     */
+    next() {
+        return this.itemSet.shift();
+    }
+
+    /**
+     * Boolean check if the FIFO Queue has remaining items.
+     *
+     * @returns {boolean}
+     */
+    hasItem() {
+        return !!this.itemSet.length;
+    }
+
+    /**
+     * Get numeric count of remaining FIFO Queue items.
+     *
+     * @returns {Number}
+     */
+    getLength() {
+        return this.itemSet.length;
+    }
+
+    /**
+     * Apply a Filter on the ItemSet to get rid of unwanted Items.
+     *
+     * Supports simple key-value objects which apply strict filters
+     * as well as more elaborate greater-than ($gt) and less-than
+     * ($lt) operators useful for numeric properties.
+     *
+     * @param filter
+     * @returns void
+     */
+    findAndRemove(filter) {
+        if (filter === undefined || typeof filter !== 'object') {
+            return;
+        }
+
+        // console.log(_dupl)
+        // console.log(this.getItems())
+
+        // this is wrong!! don't alter an array
+        // that you're iterating over...
+        this.getItems().forEach( (item, itemKey) =>  {
+            // let removeItem = false;
+
+            Object.keys(filter).forEach( (filterKey) => {
+                if (!item[filterKey]) {
+                    return;
+                }
+
+                if (filter[filterKey] === item[filterKey]) {
+                    item.remove = true;
+                    return;
+                }
+
+                if (typeof filter[filterKey] === 'object') {
+                    let filterVal = filter[filterKey];
+                    let filterOperator = Object.keys(filterVal)[0];
+                    let filterOperand  = filterVal[filterOperator];
+                    let itemOperand    = item[filterKey];
+
+                    // @TODO: store the info about operators somewhere else
+                    if (filterOperator === '$gt') {
+                        item.remove = itemOperand > filterOperand;
+                    } else if (filterOperator === '$lt') {
+                        item.remove = itemOperand < filterOperand;
+                    }
+                }
+
+            });
+        });
+
+        // @TODO: check if there's a better way
+        let index = this.itemSet.length - 1;
+        while (index >= 0) {
+            if (this.itemSet[index].remove) {
+                this.itemSet.splice(index, 1);
+            }
+            index -= 1;
+        }
+    }
+
+    /**
+     * Make sure that the FIFO Queue gets stringified nicely
+     * if needed to be logged or printed to the stdout.
+     *
+     * @returns string
+     */
+    toString() {
+        if (this.itemSet.length === 0) {
+            return ' <empty> ';
+        }
+
+        let i = 0;
+        let out = '';
+        this.itemSet.forEach((item) => {
+            out += ` [${item._id || item.id || i++}] `;
+    });
+
+        return `<-${out}<-`;
+    }
+}
+//
+// let jobQueue = new FifoQueue;
+// let jobs = [
+//     {id: 1, title: 'do something fast',     retry: 0,   prio: 100   },
+//     {id: 2, title: 'i fail sometimes',      retry: 2,   prio: 23    },
+//     {id: 3, title: 'i can also fail',       retry: 1,   prio: 1010  },
+//     {id: 4, title: 'i fail often',          retry: 99,  prio: 87    },
+//     {id: 5, title: 'i fail therefore i am', retry: 999, prio: 9892  }
+// ];
+//
+// jobQueue.setItems(jobs);
+// console.log(jobQueue)
+//
+// jobQueue.findAndRemove({prio:{$lt: 1000}});
+// console.log(jobQueue)
+
+module.exports = FifoQueue;
